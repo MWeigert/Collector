4d4d25cd029016a154f0ac3d25aacaa9d4fa354b
Mathias Weigert <weigert.mathias@web.de>
2016-03-09 20:08:44 +0100

    First big GUI refactoring

--- app/src/main/java/ch/riverworld/collector/FilterActivity.java
+++ app/src/main/java/ch/riverworld/collector/FilterActivity.java
@@ -1,7 +1,10 @@
 package ch.riverworld.collector;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TabHost;
 
 public class FilterActivity extends AppCompatActivity {
 
@@ -9,5 +12,47 @@ public class FilterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filter);
+
+        TabHost host = (TabHost)findViewById(R.id.tabHost);
+        host.setup();
+
+        //Tab 1
+        TabHost.TabSpec spec = host.newTabSpec("Allg.");
+        spec.setContent(R.id.tab_common);
+        spec.setIndicator("Allg.");
+        host.addTab(spec);
+
+        //Tab 2
+        spec = host.newTabSpec("Buch & Spiel");
+        spec.setContent(R.id.tab_bg);
+        spec.setIndicator("Buch & Spiel");
+        host.addTab(spec);
+
+        //Tab 3
+        spec = host.newTabSpec("Film");
+        spec.setContent(R.id.tab_movie);
+        spec.setIndicator("Film");
+        host.addTab(spec);
+
+    }
+
+    public void buttonOnClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_back:
+                //button Zurueck gedrueckt
+                final Intent mainIntent=new Intent(this,MainActivity.class);
+                startActivity(mainIntent);
+                break;
+            case R.id.btn_back2:
+                //button Zurueck gedrueckt
+                final Intent mainIntent2=new Intent(this,MainActivity.class);
+                startActivity(mainIntent2);
+                break;
+            case R.id.btn_back3:
+                //button Zurueck gedrueckt
+                final Intent mainIntent3=new Intent(this,MainActivity.class);
+                startActivity(mainIntent3);
+                break;
+        }
     }
 }
