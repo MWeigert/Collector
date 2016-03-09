c8ac922a841195fcc344a08a6879ded8785323a7
Mathias Weigert <weigert.mathias@web.de>
2016-03-09 22:30:33 +0100

    GUI Delete and Rental added. Some Button functionality implemented.

--- app/src/main/java/ch/riverworld/collector/DetailsActivity.java
+++ app/src/main/java/ch/riverworld/collector/DetailsActivity.java
@@ -4,13 +4,29 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 public class DetailsActivity extends AppCompatActivity {
+    private  Switch swRent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+
+        swRent = (Switch) findViewById(R.id.sw_rent);
+        final Intent rentalIntent=new Intent(this,RentalActivity.class);
+
+        swRent.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    if (isChecked) {
+                        // The toggle is enabled
+                        startActivity(rentalIntent);
+                    }
+                }
+            });
     }
 
     public void buttonOnClick(View v) {
